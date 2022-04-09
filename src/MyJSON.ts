be9c94b6303e5731fe3b505d6bc4bb1a86a3bbeb
OceanPresent <oceanpresent@163.com>
2022-04-09 18:35:05 +0800

    feat: complish Stringify and integrate all

---
+++ src/MyJSON.ts
@@ -0,0 +1,71 @@
+import type { Value } from './type';
+import { Tokenizer } from './Tokenizer';
+export class MyJSON {
+    private tokenizer: Tokenizer
+    constructor() {
+        this.tokenizer = new Tokenizer();
+    }
+    stringify(val: Value): string {
+        if (typeof val === "boolean" || typeof val === "number"
+            || typeof val === "string") {
+            return val.toString();
+        }
+        if (val === null) {
+            return "null";
+        }
+        if (Array.isArray(val)) {
+            return this.stringifyArray(val);
+        }
+        if (typeof val === "object") {
+            return this.stringifyObject(val);
+        }
+        else {
+            throw new Error("该类型不能转换成JSON字符串！");
+        }
+    }
+    parse(text: string): Value | undefined {
+        return this.tokenizer.parse(text);
+    }
+    private stringifyObject(obj: Object): string {
+        let result = "{";
+        for (const [key, value] of Object.entries(obj)) {
+            if (Array.isArray(value)) {
+                result += `"${String(key)}":${this.stringifyArray(value)},`;
+            }
+            else if (typeof value === "object") {
+                result += `"${String(key)}":${this.stringifyObject(value)},`;
+            }
+            else if (typeof value === "boolean" || typeof value === "number"
+                || value === null) {
+                result += `"${String(key)}":${String(value)},`
+            } else if (typeof value === "string") {
+                result += `"${String(key)}":"${value}",`
+            } else {
+                throw new Error("该类型不能转换成JSON字符串！");
+            }
+        }
+        result += "}";
+        return result;
+    }
+    private stringifyArray(arr: Array<any>): string {
+        let result = "[";
+        for (const value of arr) {
+            if (Array.isArray(value)) {
+                result += `${this.stringifyArray(value)},`;
+            }
+            else if (typeof value === "object") {
+                result += `${this.stringifyObject(value)},`;
+            }
+            else if (typeof value === "boolean" || typeof value === "number"
+                || value === null) {
+                result += `${String(value)},`
+            } else if (typeof value === "string") {
+                result += `"${value}",`
+            } else {
+                throw new Error("该类型不能转换成JSON字符串！");
+            }
+        }
+        result += "]";
+        return result;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { Tokenizer } from './Tokenizer';
 
-const myTokenizer = new Tokenizer();
-myTokenizer.parse("123");
+import { MyJSON } from './MyJSON';
+
+let test = new MyJSON();
+test.parse(`{"a":123,"v":true}`);
+test.stringify([1, 2, 3, { a: 123 }]);
